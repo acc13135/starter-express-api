02f915a64388941c20e6ee555520e876ec3b93d3
ehabatef205 <ehabmahmoud205@gmail.com>
2023-08-17 13:00:07 +0300

    update3

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import express from "express";
 import cors from "cors";
 import * as paypal from "./paypal-api.js";
@@ -36,48 +35,3 @@ app.post("/my-server/capture-paypal-order", async (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}/`);
 });
-=======
-const express = require('express')
-const app = express()
-const port = 7050
-const mongoose = require('mongoose')
-const router = require('./src/routes/index')
-const cors = require('cors')
-require('dotenv').config()
-var url = process.env.db
-
-const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(url)
-        console.log("MongoDB connected:  ", conn.connection.host)
-    } catch (error) {
-        console.log(error)
-        process.exit(1)
-    }
-}
-
-app.use(express.json({ limit: '50mb' }));
-
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    next();
-});
-
-app.use(router)
-
-const corsOptions = {
-    origin: "*",
-    credentials: true,
-    optionSuccessStatus: 200,
-}
-
-app.use(cors(corsOptions))
-
-connectDB().then(() => {
-    app.listen(port, () => {
-        console.log(`server is starting at port ${port}`)
-    })
-})
->>>>>>> 4bccf54f6bba316966c54c4a63b1c2bb1071c630
